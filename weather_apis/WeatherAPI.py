7145765b606819c3b8bda286b502d27c07721d19
Omar Afifi <omarafifi@windowslive.com>
2024-11-08 00:51:03 +0200

    create API classes

--- weather_apis/WeatherAPI.py
+++ weather_apis/WeatherAPI.py
@@ -1,11 +1,32 @@
 from .BaseWeatherAPI import BaseWeatherAPI
+from requests import request
+import os
 
 class WeatherAPI(BaseWeatherAPI):
     def __init__(self, *args, **kwargs):
-        pass
+        self.base_url = 'http://api.weatherapi.com/v1/'
+        self.key = os.getenv('WEATHER_API_KEY')
 
     def get_current_weather(self, city):
-        pass
+        params = {
+            'q': city,
+            'key': self.key,
+        }
 
-    def get_previous_weather(self, city):
-        pass
+        url = self.base_url + '/current.json'
+
+        response = request("GET", url=url, params=params)
+        return response.json()
+
+    def get_previous_weather(self, city, start_date, end_date):
+        params = {
+            'q': city,
+            'key': self.key,
+            'dt': start_date,
+            'end_dt': end_date,
+        }
+
+        url = self.base_url + '/history.json'
+
+        response = request("GET", url=url, params=params)
+        return response.json()
--- weather_apis/WeatherBit.py
+++ weather_apis/WeatherBit.py
@@ -1,11 +1,32 @@
 from .BaseWeatherAPI import BaseWeatherAPI
+from requests import request
+import os
 
 class WeatherBit(BaseWeatherAPI):
     def __init__(self, *args, **kwargs):
-        pass
+        self.base_url = 'https://api.weatherbit.io/v2.0'
+        self.key = os.getenv('WEATHER_BIT_KEY')
 
     def get_current_weather(self, city):
-        pass
+        params = {
+            'city': city,
+            'key': self.key,
+        }
 
-    def get_previous_weather(self, city):
-        pass
+        url = self.base_url + '/current'
+
+        response = request("GET", url=url, params=params)
+        return response.json()
+
+    def get_previous_weather(self, city, start_date, end_date):
+        params = {
+            'city': city,
+            'key': self.key,
+            'start_date': start_date,
+            'end_date': end_date,
+        }
+
+        url = self.base_url + '/history/hourly'
+
+        response = request("GET", url=url, params=params)
+        return response.json()
