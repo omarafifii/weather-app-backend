ddec524acef24a67539bb559ea044c28480562e4
Omar Afifi <omarafifi@windowslive.com>
2024-11-08 14:27:52 +0200

    logging errors

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from logic import Logic_Handler
+import traceback
 
 app = Flask(__name__)
 
@@ -14,6 +15,8 @@ def get_weather(city):
         data = logic_handler.calculate_weather_data(city)
         return data
     except Exception as e:
+        print(Exception, e)
+        print(traceback.format_exc())
         return {'message': 'An error occured'}, 400
 
 if __name__ == '__main__':
