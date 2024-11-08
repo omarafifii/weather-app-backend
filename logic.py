08386c3b91860ff492e4cb0c4c5119dbc8b104bb
Omar Afifi <omarafifi@windowslive.com>
2024-11-08 02:01:15 +0200

    update logic

--- logic.py
+++ logic.py
@@ -1,4 +1,6 @@
-from weather_apis import WeatherBit, WeatherAPI
+from weather_apis.WeatherAPI import WeatherAPI
+from weather_apis.WeatherBit import WeatherBit
+from datetime import datetime, timedelta
 
 class Logic_Handler:
     
@@ -7,16 +9,53 @@ class Logic_Handler:
         self.weather_bit = WeatherBit()
 
     def calculate_weather_data(self, city):
-        self.weather_api.get_current_weather(city)
+        yesterday_date = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')
+        today_date = (datetime.today()).strftime('%Y-%m-%d')
+        time_now = datetime.now().strftime('%H')
 
-    def calculate_current_data(self, city):
-        pass
+        weatherbit_current = self.weather_bit.get_current_weather(city)
+        weatherapi_current = self.weather_api.get_current_weather(city)
 
-    def calculate_previous_data(self, city):
-        pass
+        weatherbit_history = self.weather_bit.get_previous_weather(city, yesterday_date, today_date)
+        weatherapi_history = self.weather_api.get_previous_weather(city, yesterday_date, today_date)
 
-    def combine_api_data(self, city):
-        pass
+        weatherbit_current_clean = self.weather_bit.extract_current_data(weatherbit_current)
+        weatherapi_current_clean = self.weather_api.extract_current_data(weatherapi_current)
 
-    def get_forecast_from_previous_data(self, city):
+        weatherbit_history_clean = self.weather_bit.extract_previous_data(weatherbit_history, time_now)
+        weatherapi_history_clean = self.weather_api.extract_previous_data(weatherapi_history, time_now)
+
+        current_combined = self.combine_current_data(weatherapi_current_clean, weatherbit_current_clean)
+
+        history_combined = self.combine_previous_data(weatherapi_history_clean, weatherbit_history_clean)
+
+        forcast = self.get_forecast_from_previous_data(current_combined, history_combined)
+
+        return {
+            'current': current_combined,
+            'history': history_combined,
+            # 'forecast': forcast,
+        }
+
+    def combine_current_data(self, api1, api2):
+        result = self.combine_data_from_2_dicts(api1, api2)
+        return result
+
+    def combine_previous_data(self, api1, api2):
+        result = {}
+
+        for key in api1:
+            result[key] = self.combine_data_from_2_dicts(api1[key], api2[key])
+
+        return result
+
+    def combine_data_from_2_dicts(self, dict1, dict2):
+        result = {}
+        for key in dict1:
+            percent_change = abs((dict1[key] - dict2[key]) / dict1[key]) * 100
+            result[key] = dict1[key] * 0.6 + dict2[key] * 0.4 if percent_change < 15 else dict1[key]
+        
+        return result
+
+    def get_forecast_from_previous_data(self, current_data, history_data):
         pass
